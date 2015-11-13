172c790d43b58d7ade5cd9d4848ca28768aa62fb
Markus Degenhardt <degenhardt.markus@gmail.com>
2015-11-13 11:47:39 +0100

    main menu

---
+++ Assets/Script/MenuManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour
+{
+	// Currently open menu. Set the initial reference in the Inspector 
+	public Menu currentMenu;
+
+	void Start ()
+	{
+		ShowMenu (currentMenu); // For showing the main menu 
+	}
+	// Activate the given menu and deactivate the current 
+	// Invokable by the Inspector
+	public void ShowMenu (Menu menu)
+	{
+		if (currentMenu != null) 
+			currentMenu.IsOpen = false;
+		currentMenu = menu;
+		currentMenu.IsOpen = true; 
+	}
+
+	public void Quit ()
+	{
+		Application.Quit (); 
+		UnityEditor.EditorApplication.isPlaying = false;
+	}
+
+	public void ShowScene (int level)
+	{
+		if (currentMenu != null)
+			currentMenu.IsOpen = false;
+		currentMenu = null;
+		Application.LoadLevel (level);
+	}
+
+}
