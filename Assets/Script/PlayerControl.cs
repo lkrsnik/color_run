915786511fba91602553d102c6b64d3f98fb9413
Luka <krsnik.luka92@gmail.com>
2015-11-16 11:56:16 +0100

    Added ball movement and trail

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		float moveH = Input.GetAxis ("Horizontal");
+		float moveV = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveH, 0.0f, moveV);
+
+		rb.AddForce (movement * speed);
+	}
+}
